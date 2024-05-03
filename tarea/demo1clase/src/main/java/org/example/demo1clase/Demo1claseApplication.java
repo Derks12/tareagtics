eff3aa03be3dcb5d60edf3d12866e6a872e47567
Derks12 <148168249+Derks12@users.noreply.github.com>
2024-05-03 17:23:53 -0500

    a

---
+++ tarea/demo1clase/src/main/java/org/example/demo1clase/Demo1claseApplication.java
@@ -0,0 +1,13 @@
+package org.example.demo1clase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1claseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1claseApplication.class, args);
+    }
+
+}
---
+++ tarea/demo1clase/src/test/java/org/example/demo1clase/Demo1claseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demo1clase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1claseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
